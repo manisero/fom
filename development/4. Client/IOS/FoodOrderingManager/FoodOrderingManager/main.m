c750107c16faabf92e97a98dd3da261e4067714f
Jakub Turek <jkbturek@gmail.com>
2013-12-07 17:11:17 +0100

    Initial commit for iOS client

---
+++ development/4. Client/IOS/FoodOrderingManager/FoodOrderingManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FoodOrderingManager
+//
+//  Created by Jakub Turek on 07.12.2013.
+//  Copyright (c) 2013 Jakub Turek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FOMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FOMAppDelegate class]));
+    }
+}
---
+++ development/4. Client/IOS/FoodOrderingManager/FoodOrderingManagerTests/FoodOrderingManagerTests.m
@@ -0,0 +1,34 @@
+//
+//  FoodOrderingManagerTests.m
+//  FoodOrderingManagerTests
+//
+//  Created by Jakub Turek on 07.12.2013.
+//  Copyright (c) 2013 Jakub Turek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FoodOrderingManagerTests : XCTestCase
+
+@end
+
+@implementation FoodOrderingManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
