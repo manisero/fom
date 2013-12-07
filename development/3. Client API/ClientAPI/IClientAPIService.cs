3d18c1019737e8619d053afdf9053240cd570a82
manisero <michalaniserowicz@gmail.com>
2013-12-07 17:37:08 +0100

    Created ClientAPI project

---
+++ development/3. Client API/ClientAPI/IClientAPIService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using ClientAPI.Contract;
+
+namespace ClientAPI
+{
+    [ServiceContract]
+    public interface IClientAPIService
+    {
+        List<Restaurant> GetAllRestaurants();
+    }
+}
