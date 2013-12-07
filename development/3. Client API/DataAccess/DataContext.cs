1bd67c31aee8497983c499b1477921510452c33a
manisero <michalaniserowicz@gmail.com>
2013-12-07 15:59:59 +0100

    Created DataAccess project

---
+++ development/3. Client API/DataAccess/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using Domain;
+
+namespace DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+    }
+}
